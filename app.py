7fbd4638309501efbb1491424846bc1aac9b3a5c
Hamza Jafri <easycode04022000@gmail.com>
2024-10-07 17:59:25 +0500

    error resolved

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+
 def add(a,b):
     return a+b
 def sub(a,b,c):
     return a-b-c
-=======
+
 def add(a,b,c):
     return a+b+c
 def sub(a,b):
     return a-b
->>>>>>> 6bf17692adae61a542161fa65af29155c26c3f9a
+
 
 
 
